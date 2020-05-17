1d4706b41bf51649cb67fde67bba2f4343eb77e4
Cicero Monteiro <cicerolmonteiro@gmail.com>
2020-05-17 12:46:05 -0300

    Adding the frontend react to interaction with app

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CoffeeShopsList from './CoffeeShopsList';
+import CoffeeShopEdit from './CoffeeShopEdit';
+import Api from './Api';
+import NavBar from './NavBar';
+const api = new Api();
+
+class App extends React.Component {
+  render() {
+    const navbar = <NavBar/>;
+    return (
+      <Router>
+        <Switch>
+          <Route
+            path='/'
+            exact={true}
+            render={(props) => <Home {...props} api={api} navbar={navbar}/>}
+          />
+          <Route
+            path='/coffee-shops'
+            exact={true}
+            render={(props) => <CoffeeShopsList {...props} api={api} navbar={navbar}/>}
+          />
+          <Route
+            path='/coffee-shops/:id'
+            render={(props) => <CoffeeShopEdit {...props} api={api} navbar={navbar}/>}
+          />
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ client/src/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Link } from 'react-router-dom';
+import { Button, Container } from 'reactstrap';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        {this.props.navbar}
+        <Container fluid>
+          <div>
+            <Button color="secondary">
+              <Link className="app-link" to="/coffee-shops">Manage Coffee Shops</Link>
+            </Button>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+export default Home;
